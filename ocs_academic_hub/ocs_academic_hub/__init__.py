72c66ad9950d4441d71085bd1e83b8d0fb9ff774
Christian Foisy <cfoisy@osisoft.com>
2020-02-19 21:06:38 +0000

    initial checkin - v0.61.0, pre-gen2

---
+++ ocs_academic_hub/ocs_academic_hub/__init__.py
@@ -0,0 +1,18 @@
+#
+__version__ = "0.61.0"
+
+
+def oah_version():
+    return __version__
+
+
+from .ocs_academic_hub import HubClient
+from .hub_dataview import HubDataview
+from .omf_client import OMFClient
+
+from .util import timer, get_last_runtime, debug_requests_on, debug_requests_off
+
+try:
+    from ddtrace import tracer
+except ImportError:
+    from .dddummy import tracer 
