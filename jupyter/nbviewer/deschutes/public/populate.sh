ecb5799acf7e9cf1c49609a8b2ce2c0b8c31298a
Christian Foisy <cfoisy@osisoft.com>
2020-09-30 11:09:47 -0400

    public structure

--- jupyter/nbviewer/deschutes/public/populate.sh
+++ jupyter/nbviewer/deschutes/public/populate.sh
@@ -4,4 +4,7 @@ cp ../../../../jhub-content/osisoft-academic/Learning_Modules/Deschutes_Dataset_
 cp ../../../../jhub-content/osisoft-academic/Learning_Modules/PandasGuide.ipynb .
 cp ../../../../jhub-content/osisoft-academic/Learning_Modules/Hub_Library_Quickstart.* .
 cp *.py *.ipynb ..
-rm ../NB*SOLUTION*
+# rm ../NB*SOLUTION*
+zip -r -9 deschutes-learning-modules.zip *.ipynb *.py 
+echo
+echo "=====> upload zip and quickstart notebook+py to hub blob"
