5b9c510113c35f3da93f058e933ab6e38ca59231
Christian Foisy <cfoisy@osisoft.com>
2021-08-12 16:42:56 -0400

    match auth0 action with app_metadata

--- portal/auth/app/auth0_resp.py
+++ portal/auth/app/auth0_resp.py
@@ -31,128 +31,138 @@ AUTH0_CALLBACK_URL = env.get(constants.AUTH0_CALLBACK_URL)
 AUTH0_CLIENT_ID = env.get(constants.AUTH0_CLIENT_ID)
 AUTH0_CLIENT_SECRET = env.get(constants.AUTH0_CLIENT_SECRET)
 AUTH0_DOMAIN = env.get(constants.AUTH0_DOMAIN)
-AUTH0_BASE_URL = 'https://' + AUTH0_DOMAIN
+AUTH0_BASE_URL = "https://" + AUTH0_DOMAIN
 AUTH0_AUDIENCE = env.get(constants.AUTH0_AUDIENCE)
 AUTH0_ROLES_KEY = "https://data.academic.osisoft.com/roles"
 auth0_debug = int(env.get(constants.AUTH0_DEBUG, 0))
 print("debug:", auth0_debug)
 
-app = Flask(__name__, static_url_path='/public', static_folder='./public')
+app = Flask(__name__, static_url_path="/public", static_folder="./public")
 app.secret_key = constants.SECRET_KEY
 app.debug = True
 
-app.r = redis.StrictRedis(host=env.get("REDIS_HOST"), port=6380, db=1, password=env.get("REDIS_PASSWORD"), ssl=True)
+app.r = redis.StrictRedis(
+    host=env.get("REDIS_HOST"),
+    port=6380,
+    db=1,
+    password=env.get("REDIS_PASSWORD"),
+    ssl=True,
+)
+
 
 @app.errorhandler(Exception)
 def handle_auth_error(ex):
     response = jsonify(message=str(ex))
-    response.status_code = (ex.code if isinstance(ex, HTTPException) else 500)
+    response.status_code = ex.code if isinstance(ex, HTTPException) else 500
     return response
 
 
 oauth = OAuth(app)
 
 auth0 = oauth.register(
-    'auth0',
+    "auth0",
     client_id=AUTH0_CLIENT_ID,
     client_secret=AUTH0_CLIENT_SECRET,
     api_base_url=AUTH0_BASE_URL,
-    access_token_url=AUTH0_BASE_URL + '/oauth/token',
-    authorize_url=AUTH0_BASE_URL + '/authorize',
+    access_token_url=AUTH0_BASE_URL + "/oauth/token",
+    authorize_url=AUTH0_BASE_URL + "/authorize",
     client_kwargs={
-        'scope': 'openid profile email app_metadata',
+        "scope": "openid profile email app_metadata",
     },
 )
-
 
 
 def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         if constants.PROFILE_KEY not in session:
-            return redirect('/auth/login')
+            return redirect("/auth/login")
         return f(*args, **kwargs)
 
     return decorated
 
 
 # Controllers API
-@app.route('/')
+@app.route("/")
 def home():
-    hub_session_id = request.args.get('hub-id')
+    hub_session_id = request.args.get("hub-id")
     if hub_session_id:
-        session['hub-session-id'] = hub_session_id
-    return render_template('home.html')
+        session["hub-session-id"] = hub_session_id
+    return render_template("home.html")
 
 
-@app.route('/callback')
+@app.route("/callback")
 def callback_handling():
-    try: 
+    try:
         token = auth0.authorize_access_token()
     except:
         return "You're not part of the Academic Hub organization", 400
 
-    resp = auth0.get('userinfo')
+    resp = auth0.get("userinfo")
     userinfo = resp.json()
 
     session[constants.JWT_PAYLOAD] = userinfo
     session[constants.PROFILE_KEY] = {
-        'user_id': userinfo['sub'],
-        'name': userinfo['name'],
-        'picture': userinfo['picture'],
+        "user_id": userinfo["sub"],
+        "name": userinfo["name"],
+        "picture": userinfo["picture"],
         # 'app_metadata': userinfo['app_metadata'],
-        'token': {"token": token}
+        "token": {"token": token},
     }
 
-    hub_session_id = session.get('hub-session-id')
+    hub_session_id = session.get("hub-session-id")
     if hub_session_id:
         app.r.set("hub:" + hub_session_id, json.dumps(userinfo), 120)
         app.r.set("hub:" + hub_session_id + ":token", str(token), 120)
 
-    return redirect('/auth/dashboard')
+    return redirect("/auth/dashboard")
 
 
-@app.route('/login')
+@app.route("/login")
 def login():
-    return auth0.authorize_redirect(redirect_uri=AUTH0_CALLBACK_URL)  # , audience=AUTH0_AUDIENCE)
+    return auth0.authorize_redirect(
+        redirect_uri=AUTH0_CALLBACK_URL
+    )  # , audience=AUTH0_AUDIENCE)
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
     session.clear()
-    params = {'returnTo': url_for('home', _external=True), 'client_id': AUTH0_CLIENT_ID}
-    return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
+    params = {"returnTo": url_for("home", _external=True), "client_id": AUTH0_CLIENT_ID}
+    return redirect(auth0.api_base_url + "/v2/logout?" + urlencode(params))
 
 
-@app.route('/dashboard')
+@app.route("/dashboard")
 @requires_auth
 def dashboard():
-    roles = [i for i in session[constants.JWT_PAYLOAD][AUTH0_ROLES_KEY] if "client-" not in i]
-    return render_template('dashboard.html',
-                           userinfo=session[constants.PROFILE_KEY],
-                           userinfo_pretty=roles,  # json.dumps(session[constants.JWT_PAYLOAD], indent=4),
-                           token_pretty=json.dumps(session[constants.PROFILE_KEY]['token'], indent=4),
-                           debug=auth0_debug)
+    roles = session[constants.JWT_PAYLOAD][AUTH0_ROLES_KEY]
+    pretty_roles = {i: roles[i] for i in roles if "client-" not in i}
+    return render_template(
+        "dashboard.html",
+        userinfo=session[constants.PROFILE_KEY],
+        userinfo_pretty=pretty_roles,  # json.dumps(session[constants.JWT_PAYLOAD], indent=4),
+        token_pretty=json.dumps(session[constants.PROFILE_KEY]["token"], indent=4),
+        debug=auth0_debug,
+    )
 
 
-@app.route('/token')
+@app.route("/token")
 def return_token():
-    hub_session_id = request.headers.get('hub-id')
+    hub_session_id = request.headers.get("hub-id")
     if hub_session_id:
         try:
             token = app.r.get("hub:" + hub_session_id)
             if token is None:
                 return f'key {"hub:" + hub_session_id} not found"', 400
             else:
-                return token.decode('utf-8')
+                return token.decode("utf-8")
         except:
             return f"not found", 400
     else:
         return "no token", 400
 
 
-api.mount('/auth', app)
+api.mount("/auth", app)
 
 if __name__ == "__main__":
-    api.run(address="0.0.0.0", port=3000, debug=True) 
- 
+    api.run(address="0.0.0.0", port=3000, debug=True)
